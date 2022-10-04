e331b5ca2802ac150bcdd000ab03b96c70ead405
Crowdbotics <team@crowdbotics.com>
2022-10-04 02:06:04 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "lively_wildflower_37213",
+  url: "https://lively_wildflower_37213.botics.co",
+  api: "https://lively_wildflower_37213.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
